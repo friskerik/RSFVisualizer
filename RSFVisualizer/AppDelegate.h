05c0efd8a89f0d830f93a21903fddf721a5f8ef4
Erik Frisk <frisk@svartgul-2.local>
2015-01-12 19:22:53 +0100

    Initial Commit

---
+++ RSFVisualizer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RSFVisualizer
+//
+//  Created by Erik Frisk on 12/01/15.
+//  Copyright (c) 2015 Erik Frisk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RSFVisualizer/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  RSFVisualizer
+//
+//  Created by Erik Frisk on 12/01/15.
+//  Copyright (c) 2015 Erik Frisk. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
+}
+
+@end
